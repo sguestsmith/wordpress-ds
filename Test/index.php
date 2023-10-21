83f4d79ac4bd7c39b070f39fbc09e232ade9bab1
Stuart Guest-Smith <sguestsmith@gmail.com>
2023-10-21 19:00:35 +0000

    Updating to include new file

---
+++ Test/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width">
+ <title>Animals</title>
+ <style>
+  body * {
+   text-align: center;
+  }
+  header, article, footer {
+   margin: auto;
+   max-width: 960px;
+  }
+  footer {
+   margin-top: 3em;
+  }
+  a {
+   color: blue;
+  }
+  article img {
+   width: 100%;
+  }
+</style>
+</head>
+<body>
+ <header>
+  <h1><a href="index.php">Animals</a></h1>
+  <p>
+   <a href="cow.php">Cow</a> |
+   <a href="dog.php">Dog</a> |
+   <a href="leopard.php">Leopard</a>
+  </p>
+ </header>
+
+ <article><h2>Welcome</h2></article>
+ <!--
+ <article><img src="picsum.photos/id/237/960/300" /></article>
+ <article><img src="picsum.photos/id/200/960/300" /></article>
+ <article><img src="picsum.photos/id/219/960/300" /></article>
+ -->
+ <footer>
+  &copy; 2023
+ </footer>
+</body>
+</html>
